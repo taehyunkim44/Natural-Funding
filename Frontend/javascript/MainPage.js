c15c1f17ac324f841f740dd16ac869529a0ffd62
taehyunkim44 <eiiyy12@nate.com>
2024-04-08 20:39:07 +0900

    중간 커밋

--- Frontend/javascript/MainPage.js
+++ Frontend/javascript/MainPage.js
@@ -20,3 +20,30 @@ function nextSlide() {
   currentIndex = currentIndex === 2 ? 0 : currentIndex + 1;
   showSlide();
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  showSlide();
+});
+
+let currentIndex2 = 0;
+
+function showSlide2() {
+  const slides = document.querySelectorAll(".Homework_Items");
+  slides.forEach((slide, index) => {
+    if (index === currentIndex2) {
+      slide.style.display = "flex";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+function prevSlide2() {
+  currentIndex2 = currentIndex2 === 0 ? 2 : currentIndex2 - 1;
+  showSlide2();
+}
+
+function nextSlide2() {
+  currentIndex2 = currentIndex2 === 2 ? 0 : currentIndex2 + 1;
+  showSlide2();
+}
